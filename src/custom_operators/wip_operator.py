9cdf1d6fa73cd14413bfd1273e198c12a0d6eaa0
Adam Manhercz <42343972+AdamManhercz@users.noreply.github.com>
2025-02-16 20:18:55 +0100

    Add docstring

--- src/custom_operators/wip_operator.py
+++ src/custom_operators/wip_operator.py
@@ -1,12 +1,45 @@
-from custom_operators.github_base_operator import GitHubOperator
+"""WIPOperator Module
+
+This module defines a custom Airflow operator, `WIPOperator`, which extends the `GitHubOperator` class.
+It is used to retrieve pull requests from a GitHub repository
+and filter them to identify Work In Progress (WIP) pull requests,
+commonly represented by draft pull requests.
+
+Classes:
+    WIPOperator: An Airflow operator to retrieve and filter draft pull requests from a GitHub repository.
+"""
+from .github_base_operator import GitHubOperator
 
 
 class WIPOperator(GitHubOperator):
+    """Custom Airflow Operator to retrieve draft pull requests from GitHub.
 
+    This operator extends `GitHubOperator` and retrieves pull requests from a GitHub repository.
+    It filters the pull requests to identify Work In Progress (WIP) PRs, which are marked as drafts.
+
+    Inherits attributes from `GitHubOperator`.
+    """
     def __init__(self, **kwargs):
+        """Initializes the WIPOperator.
+
+         Args:
+             **kwargs: Additional keyword arguments passed to `GitHubOperator` and `BaseOperator`.
+         """
         super().__init__(**kwargs)
 
     def execute(self, context: dict) -> list:
+        """Executes the retrieval and filtering of draft pull requests from GitHub.
+           Retrieves pull requests from the GitHub repository and filters them to return those marked as drafts (WIP).
+
+           Args:
+               context (dict): Airflow task execution context.
+
+           Returns:
+               list: A list of dictionaries representing pull requests from the repository. Draft PRs are logged separately.
+
+           Raises:
+               Exception: If the retrieval process encounters an error.
+           """
         try:
             pull_data = self.retrieve_data()
             wip_prs = [pr for pr in pull_data if pr["draft"]]
