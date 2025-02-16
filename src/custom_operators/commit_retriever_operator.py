21d60e1f7eab8cc987eb571251fc7a0375dd6777
Adam Manhercz <42343972+AdamManhercz@users.noreply.github.com>
2025-02-16 20:17:21 +0100

    Update docstring

--- src/custom_operators/commit_retriever_operator.py
+++ src/custom_operators/commit_retriever_operator.py
@@ -1,9 +1,37 @@
-from custom_operators.github_base_operator import GitHubOperator
+"""CommitRetrieverOperator Module.
+
+This module defines a custom Airflow operator, `CommitRetrieverOperator`, which extends the `GitHubOperator` class.
+It is used to retrieve and filter commits from a GitHub repository based on specified criteria such as branch name,
+commit author, and commit date.
+
+Classes:
+    CommitRetrieverOperator: An Airflow operator to retrieve and filter commits from a GitHub repository.
+"""
+from .github_base_operator import GitHubOperator
 
 
 class CommitRetrieverOperator(GitHubOperator):
+    """Custom Airflow Operator to retrieve and filter GitHub commits.
 
+    This operator extends `GitHubOperator` and retrieves commits from a GitHub repository. It allows
+    filtering based on branch name, commit author, and commit date.
+
+    Attributes:
+        branch_name (str): Optional; branch name to filter commits.
+        commit_author (str): Optional; author name to filter commits.
+        commit_date (str): Optional; date (YYYY-MM-DD) to filter commits.
+        endpoint (str): GitHub API endpoint for retrieving commits.
+        Inherits other attributes from `GitHubOperator`.
+    """
     def __init__(self, branch_name: str = None, commit_author: str = None, commit_date: str = None, **kwargs):
+        """Initializes the CommitRetrieverOperator with optional filtering criteria.
+
+        Args:
+            branch_name (str, optional): Name of the branch to filter commits.
+            commit_author (str, optional): Author name to filter commits.
+            commit_date (str, optional): Date (YYYY-MM-DD) to filter commits.
+            **kwargs: Additional keyword arguments passed to `GitHubOperator` and `BaseOperator`.
+        """
         super().__init__(**kwargs)
         self.branch_name = branch_name
         self.commit_author = commit_author
@@ -11,6 +39,20 @@ class CommitRetrieverOperator(GitHubOperator):
         self.endpoint = "commits?sha={}".format(self.branch_name) if self.branch_name else "commits"
 
     def execute(self, context: dict) -> list:
+        """Executes the retrieval and filtering of GitHub commits.
+
+        Retrieves commits from the GitHub repository and filters them based on the provided
+        branch name, commit author, and commit date.
+
+        Args:
+            context (dict): Airflow task execution context.
+
+        Returns:
+            list: A list of dictionaries representing filtered GitHub commits.
+
+        Raises:
+            Exception: If the retrieval or filtering process encounters an error.
+        """
         try:
             commits_data = self.retrieve_data()
             commits = [commit for commit in commits_data
